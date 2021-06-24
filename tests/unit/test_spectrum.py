ff012fde34833d7c831154eba8f9f6893b1864bd
Marc Jones <marc.jones@crick.ac.uk>
2021-06-24 14:59:35 +0100

    Added test to bin the test spectrum

--- tests/unit/test_spectrum.py
+++ tests/unit/test_spectrum.py
@@ -1,5 +1,6 @@
 import glycoproteomics
 import numpy as np
+import os
 
 
 def test_bin_spectrum():
@@ -59,6 +60,57 @@ def test_bin_spectrum():
                 "168.066": 7.5,
             }
         },
+    }
+
+
+def test_bin_test_spectrum(rootdir):
+    spectrum_path = os.path.join(rootdir, "tests", "data", "spectrum.txt.gz")
+    spectrum = glycoproteomics.io.read_spectrum_file(spectrum_path)
+    binned_spectrum = glycoproteomics.spectrum.bin(spectrum, 1.0, 100.0, np.mean)
+    assert list(binned_spectrum.keys()) == [
+        0.5,
+        1.5,
+        2.5,
+        3.5,
+        4.5,
+        5.5,
+        6.5,
+        7.5,
+        8.5,
+        9.5,
+        10.5,
+        11.5,
+        12.5,
+        13.5,
+        14.5,
+    ]
+    assert list(binned_spectrum[0.5].keys()) == [
+        750.0,
+        850.0,
+        950.0,
+        1050.0,
+        1150.0,
+        1250.0,
+        1350.0,
+        1450.0,
+        1550.0,
+        1650.0,
+    ]
+    assert binned_spectrum[0.5][750.0] == {
+        "204.087": 0.0,
+        "186.076": 0.0,
+        "168.066": 0.0,
+        "366.139": 0.0,
+        "144.066": 0.0,
+        "138.055": 0.0,
+        "512.197": 0.007032763636363635,
+        "292.103": 0.0,
+        "274.092": 0.0,
+        "657.235": 0.0,
+        "243.026": 0.0,
+        "405.079": 0.0,
+        "485.046": 0.0,
+        "308.098": 0.0,
     }
 
 
